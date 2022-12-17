9ec7b40b77861620560d1f8af39dc2e15c32c414
Sapthagiri Sangem <ssangemcodes@gmail.com>
2022-12-17 10:15:20 -0600

    exception handling and arrays and collections

---
+++ ArraysCollections/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysCollections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //var input = Console.ReadLine();//input is one string value
+                                           // what if we want to hold number of values... we use an array or a list or a collection
+                                           // System.Collections.Generic; is the namespace that provides these functionalities
+
+            //Arrays
+
+            var studentArray = new Student[3]; // we have to initialize the number of elements at the time of declaration
+            studentArray[0] = new Student { StudentId = 1, StudentName = "Akhil", StudentSSN ="12345" }; //I can initialize the properties of object like this also
+            studentArray[1] = new Student { StudentId = 2, StudentName = "Jay", StudentSSN = "12346" }; //I can initialize the properties of object like this also
+            studentArray[2] = new Student { StudentId = 3, StudentName = "Suraj", StudentSSN = "12347" }; //I can initialize the properties of object like this also
+
+            Console.WriteLine("Display elements in student array");
+            Console.WriteLine(studentArray.Length);
+            foreach (var item in studentArray)
+            {
+                if (item != null)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            Console.WriteLine("----------------------------------------------------------------");
+            
+
+
+            var studentList = new List<Student>(); //we do not have to specify a capacity at the time of declaration
+            var student1 = new Student { StudentId = 1, StudentName = "Akhil", StudentSSN = "12345" };
+            studentList.Add(student1);
+            var student2 = new Student { StudentId = 2, StudentName = "Jay", StudentSSN = "12346" };
+            studentList.Add(student2);
+            //if(!studentList.Contains(student2)) //! it negates... if the student item is not existing then only add it
+                studentList.Add(new Student { StudentId = 12, StudentName = "Jay", StudentSSN = "8887778888" });
+            studentList.Add(new Student { StudentId = 3, StudentName = "Suraj", StudentSSN = "12347" });
+            studentList.Add(new Student { StudentId = 4, StudentName = "Ram", StudentSSN = "12348" });
+            studentList.Add(new Student { StudentId = 5, StudentName = "John", StudentSSN = "12349" });
+            studentList.Add(new Student { StudentId = 6, StudentName = "Sam", StudentSSN = "12390" });
+            Console.WriteLine("Display elements in student List");
+
+            Console.WriteLine(studentList.Count);
+            foreach (var item in studentList)
+            {
+                if (item != null)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            Console.WriteLine("----------------------------------------------------------------");
+
+            //LINQ - Language integrated query... linq to sql using entity framework
+            //today we are taking about LINQ to objects
+
+            Console.WriteLine("Which student you want to find");
+            var studentNameForQuery = Console.ReadLine(); //studentNameForQuery this type of naming variables is called camel case
+
+            var foundStudent = studentList.Where(x => x.StudentName == studentNameForQuery).ToList(); // "x => x.StudentName == studentNameForQuery" this is an example of lambda expression
+
+            ////var firstStudent = studentList.First(x => x.StudentName == studentNameForQuery);
+            //var firstStudent = studentList.Where(x => x.StudentName == studentNameForQuery).First(); //throws an exception when we cannot find the element
+
+            var firstStudent = studentList.FirstOrDefault(x => x.StudentName == studentNameForQuery);
+            //var firstStudent = studentList.Where(x => x.StudentName == studentNameForQuery).FirstOrDefault();
+
+            //what LINQ methods have you used - Where, First, FirstOrDefault.. Select, All, Any
+            //what is the difference between First and FirstOrDefault
+
+
+            foreach (var item in foundStudent)
+            {
+                Console.WriteLine(item);
+            }
+
+            if(firstStudent == null)
+            {
+                Console.WriteLine("we cannot find the student");
+            }
+
+            Console.WriteLine(studentList.All(x=> !string.IsNullOrWhiteSpace(x.StudentName)));
+            Console.WriteLine(studentList.Any(x=> x.StudentId > 13));
+
+            Console.ReadLine();
+
+        }
+
+
+    }
+
+    public class Student //Encapsulation - grouping related data together
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string StudentSSN { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1} {2}", StudentId, StudentName, StudentSSN);
+        }
+    }
+}
---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var handler = new ExceptionHandling();
+                handler.Demo();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exception happened in the code. Handled in the Main method.");
+            }
+            Console.ReadLine();
+        }
+    }
+
+    public class ExceptionHandling
+    {
+        public void Demo()
+        {
+            //try, catch, Finally
+            //can we have multiple catch blocks. Yes.
+            try
+            {
+                Console.WriteLine("enter a number");
+
+                var num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter a divisor");
+
+                var divisor = Convert.ToInt32(Console.ReadLine());
+
+                var quotient = num1 / divisor;
+
+                Console.WriteLine(quotient);
+
+                //We have made a connection to the file and some exception happened. We need clean up the connection. We need to destroy the connection.
+
+            }
+            catch (FileNotFoundException fnnex)
+            {
+                Console.WriteLine(fnnex.Message);
+            }
+            catch (SqlException sqlex)
+            {
+                Console.WriteLine(sqlex.Message);
+            }
+            catch (DivideByZeroException ex) //super class of all .Net exceptions is Exception. Specific exceptions are FileNotFoundException, DivideByZeroException, SqlExecption, IndexOutofRangeException
+            {
+                Console.WriteLine("DivideByZeroException happened in the code"); //writing to console for our information, in real world we write it to error logs
+                Console.WriteLine(ex.Message + " " + ex.InnerException);
+                Console.WriteLine(ex.GetType());
+                throw ex; //throw keyword throws exception to the calling method
+            }
+            catch (Exception ex) //Generic exception handler, catch all
+            {
+                Console.WriteLine("Generic Exception happened in the code"); //writing to console for our information, in real world we write it to error logs
+                Console.WriteLine(ex.Message + " " + ex.InnerException);
+                Console.WriteLine(ex.GetType());
+            }
+            finally
+            {
+                Console.WriteLine("You are in finally block. This is used in general to release any resources");
+                Console.WriteLine("This will be executed even if there is no exception.");
+
+                //even when there is no exception finally block is executed. Because even in the case of no exception you have to close connections, release file locks etc.
+                //in general you have to release resources
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
