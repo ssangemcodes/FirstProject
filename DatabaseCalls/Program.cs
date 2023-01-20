22ae47399fc4548d3e998ed49ccb6fd292aa7dd5
Sapthagiri Sangem <ssangemcodes@gmail.com>
2023-01-19 20:18:56 -0600

    latest

---
+++ DatabaseCalls/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Training.DataAccess;
+
+namespace DatabaseCalls
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first name to be searched:");
+            var firstNameForSearch = Console.ReadLine();
+            var advWorksDA = new AdventureWorksDataAccess();
+            var students = advWorksDA.GetStudentByFirstName(firstNameForSearch);
+
+            foreach (var item in students)
+            {
+                Console.WriteLine(item.FirstName + " " + item.LastName + " " + item.StudentId);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DatabaseCalls/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DatabaseCalls")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("DatabaseCalls")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5228d2fb-9f16-4257-9b13-a139e642f644")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
